5915c003eaaecae7e6a16fa289224ca07d8df6a9
sdim-ua <ingersol@yandex.ua>
2019-03-05 22:24:48 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0802;
+
+/* 
+HashMap из 10 пар
+*/
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        
+    Map <String, String> map = new HashMap <String, String>();
+    map.put("арбуз","ягода");
+    map.put("банан","трава,");
+    map.put("трава,","ягода");
+    map.put("груша"," фрукт");
+    map.put("дыня","овощ");
+    map.put("ежевика","куст");
+    map.put("жень-шень","корень");
+    map.put("земляника","ягода");
+    map.put("ирис","цветок");
+    map.put("картофель","клубень.");
+        
+        
+        
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+
+   while (iterator.hasNext())
+    {
+        //получение «пары» элементов
+        Map.Entry<String, String> pair = iterator.next();
+        String key = pair.getKey();            //ключ
+        String value = pair.getValue();        //значение
+        System.out.println(key + ":" + value);
+    }
+
+    }
+}
